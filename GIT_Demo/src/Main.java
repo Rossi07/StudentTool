8fc2f85667bb6e85dcbd8d4c1386c84e029e1392
Roshan <roshankoli07@gmail.com>
2017-12-20 15:28:31 +0530

    First Commit...
    Signed-off-by: Roshan <roshankoli07@gmail.com>

---
+++ GIT_Demo/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentData student=new  StudentData();
+		student.display();
+		noticeBoard();
+	}
+	public static void noticeBoard()
+	{
+		System.out.println("School Notice");
+		System.out.println("College Notice");
+	}
+}
---
+++ GIT_Demo/src/StudentData.java
@@ -0,0 +1,11 @@
+
+public class StudentData {
+
+	String[] name={"Roshan","Raj","Rahul"};
+
+	public void display() {
+		// TODO Auto-generated method stub
+		for (int i=0;i<name.length;i++)
+			System.out.println("Names: "+name[i]);
+	}
+}
